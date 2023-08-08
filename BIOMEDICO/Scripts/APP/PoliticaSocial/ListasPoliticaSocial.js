ae38416f3f06ed6f083b7857ab19bd1bc6253c4b
Javier Ruiz Mendoza <jjavierruiz91@gmail.com>
2023-08-08 13:42:15 -0500

    Politica Social -Tabla Actualizada

--- BIOMEDICO/Scripts/APP/PoliticaSocial/ListasPoliticaSocial.js
+++ BIOMEDICO/Scripts/APP/PoliticaSocial/ListasPoliticaSocial.js
@@ -3,7 +3,7 @@
 var TablaPoliticaSocial = [];
 $(document).ready(function () {
 
-    RenderTable('datatable-politicasocial', [0, 1, 2, 3, 4, 5,6,7,8,9], null, {
+    RenderTable('datatable-politicasocial', [0, 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], null, {
         "paging": true,
         "ordering": false,
         "info": true,
@@ -53,26 +53,57 @@ function CargarTabla(data) {
                 item.SexoNacimiento,
                 item.IdentidadGenero,
                 item.OrientacionSexual,
-                //item.Discapacidad,
-                //item.GrupoEtnico,
-                //item.VictimaConflictoArmado,
-                //item.PoblacionPriorizada,
-                //item.CorreoElectronico,
-                //item.Municipios,
-                //item.Zona,
-                //item.NivelEscolaridad,
-                //item.Profesion,
-                //item.AmbienteLudico,
-                //item.AmbienteLudicoExpresa,
-                //item.AmbienteLudicoCorporal,
-                //item.FechaRegistro,
+                item.Discapacidad,
+                item.GrupoEtnico,
+                item.VictimaConflictoArmado,
+                item.PoblacionPriorizada,
+                item.CorreoElectronico,
+                item.TelefonoContacto,
+                item.Municipios,
+                item.Zona,
+                item.NivelEscolaridad,
+                item.Profesion,
+                item.AmbienteLudico,
+                item.AmbienteLudicoExpresa,
+                item.AmbienteLudicoCorporal,
+                item.FechaRegistro == undefined ? '' : JSONDateconverter(item.FechaRegistro),
+
 
                 '<i class="btn btn-danger btn-group-sm icon-trash" title="Eliminar" onclick="Eliminar(' + item.IdPoliticaSocial + ')" ></i>&ensp;' +
                 '<i class="btn btn-primary btn-group-sm fa fa-pencil-square-o" id="edit_ActEco_' + index + '" title="Modificar" style="fontsize:90px !important" onclick="ActualizardPoliticaSocial(' + item.IdPoliticaSocial + ')"></i>&ensp;' +
                 '<i class="btn btn-info btn-group-sm icon-magazine" title="Detalle" onclick="DetalleData(' + item.IdPoliticaSocial + ')" ></i>&ensp;'
             ]).draw(false);
 
+            var Discapacidad = TablaPoliticaSocial.column(9);
+            var GrupoEtnico = TablaPoliticaSocial.column(10);
+            var VictimaConflictoArmado = TablaPoliticaSocial.column(11);
+            var PoblacionPriorizada = TablaPoliticaSocial.column(12);
+            var CorreoElectronico = TablaPoliticaSocial.column(13);
+            var TelefonoContacto = TablaPoliticaSocial.column(14);
+            var Municipios = TablaPoliticaSocial.column(15);
+            var Zona = TablaPoliticaSocial.column(16);
+            var NivelEscolaridad = TablaPoliticaSocial.column(17);
+            var Profesion = TablaPoliticaSocial.column(18);
+            var AmbienteLudico = TablaPoliticaSocial.column(19);
+            var AmbienteLudicoExpresa = TablaPoliticaSocial.column(20);
+            var AmbienteLudicoCorporal = TablaPoliticaSocial.column(21);
+            var FechaRegistro = TablaPoliticaSocial.column(22);
 
+        
+        Discapacidad.visible(false);
+         GrupoEtnico.visible(false);
+          VictimaConflictoArmado.visible(false);
+          PoblacionPriorizada.visible(false);
+          CorreoElectronico.visible(false);
+          TelefonoContacto.visible(false);
+          Municipios.visible(false);
+          Zona.visible(false);
+          NivelEscolaridad.visible(false);
+          Profesion.visible(false);
+          AmbienteLudico.visible(false);
+          AmbienteLudicoExpresa.visible(false);
+          AmbienteLudicoCorporal.visible(false);
+          FechaRegistro.visible(false);
 
         
         
